54c9e8c904d35fa9cbc39919074b74f00f235cc0
Vincent Farah <sefrica@live.com>
2016-07-04 21:07:35 +0100

    Added site states and navigation

--- src/LOB.WebClient/app/app.js
+++ src/LOB.WebClient/app/app.js
@@ -14,13 +14,23 @@
     app.config(["$stateProvider",
                 "$urlRouterProvider",
         function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise("/products");
+            $urlRouterProvider.otherwise("/");
             $stateProvider
+                //Home
+                .state("home", {
+                    url: "/",
+                    templateUrl:"app/welcomeView.html"                    
+                })
                 // Products
                 .state("productList", {
                     url: "/products",
                     templateUrl: "app/products/productListView.html",
                     controller: "ProductListCtl as vm"
+                })
+                .state("productEdit", {
+                    url: "/products/edit/:productId",
+                    templateUrl: "app/products/productEditView.html",
+                    controller: "ProductEditCtl as vm"
                 });
         }
     ]);
